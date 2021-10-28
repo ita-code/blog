a95153b29a4f630990ee4b5926aaf3bce6c8a1b8
敖宇杰 <aoyujie@xuntec.cn>
2021-10-28 11:06:25 +0800

    修复关于404

--- blogs/.vuepress/config/nav/index.js
+++ blogs/.vuepress/config/nav/index.js
@@ -2,5 +2,5 @@ module.exports = [
   { text: '主页', link: '/', icon: 'reco-home' },
   { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
   { text: '留言板', link: '/views/other/messageBoard.html', icon: 'reco-suggestion' },
-  { text: '关于', link: '/blogs/about/about.md', icon: 'reco-account' },
+  { text: '关于', link: '/about/about.md', icon: 'reco-account' },
 ]
