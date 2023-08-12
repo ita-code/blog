404a9f0b6c6cc4f97283aa3818339250bf9bd3fe
ita <ita201125@gmail.com>
2023-08-12 21:22:49 +0800

    commit: 2023-08-12 21:22:49

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,14 +1,11 @@
-git pull
-
-#!/bin/bash
-
 # 设置要替换的地址和新地址
 old_address="/Users/ita/code/pic"
 new_address="https://iskr.gitee.io/pic"
 
 # 替换指定类型的文件中的地址
-find . -name "*.md" -type f -exec sed -i "s|$old_address|$new_address|g" {} +
+find /Users/ita/code/blog -type f -name "*.md" -exec sed -i "s#$old_address#$new_address#g" {} \;
 
+git pull
 
 npm run build
 
@@ -16,7 +13,8 @@ git add .
 date=`date "+%Y-%m-%d %H:%M:%S"`
 git commit -m "commit: ${date}"
 
-git push 
+git push origin
+git push github
 
 #sh脚本,一个命令执行多个操作
 # yarn build:example
