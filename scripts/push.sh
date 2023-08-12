ee910ada05474204e4f82811c99a4a45bfa33f51
ita <ita201125@gmail.com>
2023-08-12 21:39:10 +0800

    commit: 2023-08-12 21:39:10

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,11 +1,11 @@
 # 设置要替换的地址和新地址
-old_address="/Users/ita/code/pic"
+old_address="/Users/ita/code/Document/pic"
 new_address="https://iskr.gitee.io/pic"
 
 # 替换指定类型的文件中的地址
-find /Users/ita/code/blog/blogs -type f -name "*.md" -exec sed -i '' "s|$old_address|$new_address|g" {} \;
+find /Users/ita/code/Document/blog/blogs -type f -name "*.md" -exec sed -i '' "s|$old_address|$new_address|g" {} \;
 
-. /Users/ita/code/Docment/pic/push.sh
+. /Users/ita/code/Document/pic/push.sh
 
 git pull
 
