6ab8839838b42a9e9e6a6c81ec6a74cd4c051523
ita <ita201125@gmail.com>
2023-08-12 21:41:53 +0800

    commit: 2023-08-12 21:41:53

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,7 +5,7 @@ new_address="https://iskr.gitee.io/pic"
 # 替换指定类型的文件中的地址
 find /Users/ita/code/Document/blog/blogs -type f -name "*.md" -exec sed -i '' "s|$old_address|$new_address|g" {} \;
 
-. /Users/ita/code/Document/pic/push.sh
+. ../../pic/push.sh
 
 git pull
 
