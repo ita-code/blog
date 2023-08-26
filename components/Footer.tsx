d07c61d2d57c7c6def38217e130a4a86974b7a6c
ita <ita201125@gmail.com>
2023-08-27 02:30:13 +0800

    tj

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,6 +9,7 @@ export default function Footer() {
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+          <SocialIcon kind="csdn" href={siteMetadata.csdn} size={6} />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -1,4 +1,4 @@
-import { Mail, Github, Facebook, Youtube, Linkedin, Twitter, Mastodon } from './icons'
+import { Mail, Github, Facebook, Youtube, Linkedin, Twitter, Mastodon, Csdn } from './icons'
 
 const components = {
   mail: Mail,
@@ -8,6 +8,7 @@ const components = {
   linkedin: Linkedin,
   twitter: Twitter,
   mastodon: Mastodon,
+  csdn:Csdn
 }
 
 type SocialIconProps = {
