b362535816bb9505ca7acf1ecb36381b19f8db7a
ita <ita201125@gmail.com>
2023-08-26 17:09:42 +0800

    升级

--- image-loader.js
+++
@@ -1,3 +0,0 @@
-export default function myImageLoader({ src, width, quality }) {
-  return `/${src}?w=${width}&q=${quality || 75}`
-}
--- src/components/Image.tsx
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line jsx-a11y/alt-text
-// eslint-disable-next-line @next/next/no-img-element
-const Image = ({ ...rest }) => <img alt="image" {...rest} />
-
-export default Image
