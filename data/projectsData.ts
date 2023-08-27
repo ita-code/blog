e3a0fb90ac4247706bc9a8d0ba6cee5b9b59bf0c
ita <ita201125@gmail.com>
2023-08-27 15:45:04 +0800

    新增video文件。直接读取app下面page页面

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,9 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '后台管理组件化解决方案',
+    description: `一款基于 Vue3.3、TypeScript、Vite4、Pinia、Element-Plus 开源的后台管理框架，使用目前最新技术栈开发。项目提供强大的 ProTable 组件，在一定程度上提高您的开发效率。另外本项目还封装了一些常用组件、Hooks、指令、动态路由、按钮级别权限控制等功能。`,
+    imgSrc: 'https://i.imgtg.com/2023/04/13/8tl1j.png',
+    href: 'https://github.com/ita-code/admin-vue3-ts',
   },
 ]
 
