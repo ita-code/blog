273be69384990cbeae5ffe12a7a7b3657563bc55
ita <ita201125@gmail.com>
2023-09-07 23:12:36 +0800

    pink

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Tag = ({ text, content }: Props) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="hover:text-primary dark:hover:text-primary ml-2 inline-flex items-center whitespace-nowrap rounded-full bg-zinc-100 px-2.5 py-0.5 text-xs leading-4 text-zinc-600 dark:bg-zinc-800 dark:text-zinc-200">
+      <a className="ml-2 inline-flex items-center whitespace-nowrap rounded-full bg-zinc-100 px-2.5 py-0.5 text-xs leading-4 text-zinc-600 hover:text-primary-500 dark:bg-zinc-800 dark:text-zinc-200 dark:hover:text-primary-400">
         # {text.split(' ').join('-')}
         {content}
       </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.indigo,
+        primary: colors.pink,
         //@ts-ignore
         gray: colors.neutral, // TODO: Remove ts-ignore after tw types gets updated to v3
       },
