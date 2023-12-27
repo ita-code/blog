6f146b0b20d781b29af1727636705abbe73a166e
ita <ita201125@gmail.com>
2023-12-27 14:00:34 +0800

    项目暂时卡片修改

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -14,7 +14,7 @@ export default function Projects() {
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">我的最近项目</p>
         </div>
-        <div className="container py-12">
+        <div className="container py-8">
           <div className="-m-4 flex flex-wrap">
             {projectsData.map((d) => (
               <Card
